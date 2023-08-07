7a8b2af0b1c9df41d6d26c875a2bd7692e8e957a
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-08-07 09:55:37 +0700

    Update pertunjukan_dina.py

--- pertunjukan_dina.py
+++ pertunjukan_dina.py
@@ -4,6 +4,7 @@
 # Programmed by : Armansyah, S.Kom, M.Pd
 # Guru Informatika SMAN Sumatera Selatan 
 # armansyah@smansumsel.sch.id
+# Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
 
 def temukan_maksimal_pertunjukan(jadwal):
     jadwal_terurut = sorted(jadwal, key=lambda x: x[1])  # Urutkan jadwal berdasarkan waktu selesai
