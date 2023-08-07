6a6e98b00e570c6326b505ac0fbf67444af0dfe2
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-08-07 13:23:08 +0700

    Create rekursinolimit.py

---
+++ rekursinolimit.py
@@ -0,0 +1,12 @@
+# Simulasi Rekursif tak terbatas
+# Buku Siswa IKM Informatika untuk SMA Kelas X Hal. 23
+# Programmed by : Armansyah, S.Kom, M.Pd
+# Guru Informatika SMAN Sumatera Selatan 
+# armansyah@smansumsel.sch.id
+# Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
+# Setiap copy-paste dan pengembangan harus mencantumkan informasi HAKI diatas
+
+def halo_dunia():
+  print('Halo dunia!')
+  # panggil dirinya sendiri
+  print(halo_dunia)
