e6e8fbb8e9e060d4d44efafa2b88c5136ea0944f
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-08-07 11:14:40 +0700

    Update pr-cici.py

--- pr-cici.py
+++ pr-cici.py
@@ -5,6 +5,7 @@
 # Guru Informatika SMAN Sumatera Selatan 
 # armansyah@smansumsel.sch.id
 # Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
+# Setiap copy-paste dan pengembangan harus mencantumkan informasi HAKI diatas
 
 def maks_nilai_knapsack(bobot, nilai, kapasitas):
     n = len(bobot)
