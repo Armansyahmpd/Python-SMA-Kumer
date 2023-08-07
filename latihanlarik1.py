44615a4635c7d1f52096e6234c5b826af28663bc
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-08-07 09:33:39 +0700

    Create latihanlarik1.py

---
+++ latihanlarik1.py
@@ -0,0 +1,25 @@
+# Aktivitas SAP-K11-08: Latihan Larik soal no 1
+# Buku Siswa IKM Informatika untuk SMA Kelas XI Hal. 48
+# Algoritma dan Pemrograman
+# Programmed by : Armansyah, S.Kom, M.Pd
+# Guru Informatika SMAN Sumatera Selatan 
+# armansyah@smansumsel.sch.id
+
+
+def main():
+    try:
+        n = int(input("Masukkan jumlah bilangan: "))
+        numbers = []
+
+        for i in range(n):
+            num = int(input(f"Masukkan bilangan ke-{i + 1}: "))
+            numbers.append(num)
+
+        print("Bilangan secara terbalik:")
+        for num in reversed(numbers):
+            print(num, end=" ")
+            # Jika ingin membuat output turun secara vertikal gunakan :
+            #  print(num)
+
+    except ValueError:
+        print("Masukan harus berupa bilangan bulat.")
