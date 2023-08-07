2696c770f0fc6831db6c52cc503b1f4d9cebad36
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-08-07 10:06:42 +0700

    Create simulasiburung.py

---
+++ simulasiburung.py
@@ -0,0 +1,25 @@
+# Problem Simulasi Burung (kasus Boro)
+# Buku Siswa IKM Informatika untuk SMA Kelas XI Hal. 53
+# Algoritma dan Pemrograman
+# Programmed by : Armansyah, S.Kom, M.Pd
+# Guru Informatika SMAN Sumatera Selatan 
+# armansyah@smansumsel.sch.id
+# Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
+
+
+import math
+
+def hitung_jarak_horizontal(sudut, kecepatan_awal):
+    sudut_radian = math.radians(sudut)
+    jarak_horizontal = (kecepatan_awal ** 2) * math.sin(2 * sudut_radian) / 10
+    return jarak_horizontal
+
+# Meminta input dari pengguna untuk sudut peluncuran dan kecepatan awal
+sudut = int(input("Masukkan Sudut Peluncuran (Nilai S) : "))
+kecepatan_awal = int(input("Masukkan Nilai Kecepatan awal : "))
+
+# Menghitung jarak horizontal
+jarak = hitung_jarak_horizontal(sudut, kecepatan_awal)
+
+# Menampilkan hasil
+print("Jarak terjauh yang dapat ditempuh oleh Boro:", jarak)
