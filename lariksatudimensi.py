53fc4ee45fed37204741efe30aada3257b022b28
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-08-07 09:54:31 +0700

    Update lariksatudimensi.py

--- lariksatudimensi.py
+++ lariksatudimensi.py
@@ -4,6 +4,7 @@
 # Programmed by : Armansyah, S.Kom, M.Pd
 # Guru Informatika SMAN Sumatera Selatan 
 # armansyah@smansumsel.sch.id
+# Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
 
 def cetak_larik(ukuran, larik):
     for i in range(ukuran):
