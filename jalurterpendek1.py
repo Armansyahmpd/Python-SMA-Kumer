ce2a4de97f897a432e093f22b3737dfceea02cc1
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-08-11 14:19:39 +0700

    Update jalurterpendek1.py

--- jalurterpendek1.py
+++ jalurterpendek1.py
@@ -1,23 +1,25 @@
-# Program Algoritma Greedy untuk mencari jalur terpendek antar percabangan ABCD 
-# Buku Siswa IKM Informatika untuk SMA Kelas XI Hal. 29
+# Mencari jalur terpendek antar titik ABCD berdasar algoritma Greedy
+# Buku Siswa IKM Informatika untuk SMA Kelas XI hal. 47
+# Algoritma dan Pemrograman
 # Programmed by : Armansyah, S.Kom, M.Pd
 # Guru Informatika SMAN Sumatera Selatan 
 # armansyah@smansumsel.sch.id
-# Alumni CS50 for Teacher Harvard-Indonesia 2022/2023
+# Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
 # Setiap copy-paste dan pengembangan harus mencantumkan informasi HAKI diatas
 
-def cari_jalur_terpendek(cabang):
-    posisi_sekarang = 'A'
+
+def cari_jalur_terpendek(cabang, posisi_awal, posisi_tujuan):
+    posisi_sekarang = posisi_awal
     jalur = [posisi_sekarang]
     total_jarak = 0
 
-    while posisi_sekarang != 'B':
+    while posisi_sekarang != posisi_tujuan:
         cabang_selanjutnya = None
         jarak_terpendek = float('inf')
 
-        for cabang, jarak in cabang[posisi_sekarang].items():
-            if cabang not in jalur and jarak < jarak_terpendek:
-                cabang_selanjutnya = cabang
+        for cabang_tetangga, jarak in cabang[posisi_sekarang].items():
+            if cabang_tetangga not in jalur and jarak < jarak_terpendek:
+                cabang_selanjutnya = cabang_tetangga
                 jarak_terpendek = jarak
 
         if cabang_selanjutnya is None:
@@ -39,6 +41,9 @@ if __name__ == "__main__":
         'D': {},
     }
 
+    posisi_awal = input("Masukkan posisi awal (A, B, C, atau D): ")
+    posisi_tujuan = input("Masukkan posisi tujuan (B, C, atau D): ")
+
     # Input panjang jalan setiap cabang
     for simpul in cabang_cadangan:
         for tetangga in cabang_cadangan[simpul]:
@@ -46,9 +51,9 @@ if __name__ == "__main__":
             jarak = int(input())
             cabang_cadangan[simpul][tetangga] = jarak
 
-    jalur_terpendek, total_jarak = cari_jalur_terpendek(cabang_cadangan)
+    jalur_terpendek, total_jarak = cari_jalur_terpendek(cabang_cadangan, posisi_awal, posisi_tujuan)
 
     if jalur_terpendek and total_jarak is not None:
-        print("Jalur terpendek dari A ke B yang harus dilalui:")
+        print(f"Jalur terpendek dari {posisi_awal} ke {posisi_tujuan} yang harus dilalui:")
         print(" -> ".join(jalur_terpendek))
         print("Total jarak yang harus ditempuh:", total_jarak)
